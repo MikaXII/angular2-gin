624f09abb0b5b7dea29ad4296eb746bea8f8659c
MikaXII <mikael.joubert@outlook.com>
2016-11-21 22:13:46 +0100

    Add sample for test api side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { DataService } from './service/sample.service';
+import { MsgSample } from "./models/sample.model"
+import { Configuration} from "./service/app.constants"
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@NgModule({
+    imports: [BrowserModule]
+})
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DataService, Configuration]
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  //title = 'app works!';
+  public title: string;
+  constructor(private _dataService: DataService){}
+
+  ngOnInit(){
+    this.getPing();
+    
+  }
+
+  private getPing(): void {
+      this._dataService
+      .GetPing()
+      .subscribe((data:MsgSample) => this.title = data.message,
+      error => console.log(error),
+      () => console.log("pong") );
+  }
 }
---
+++ src/app/models/sample.model.ts
@@ -0,0 +1,7 @@
+export class MsgSample {
+    message: string;
+
+    constructor(msgInfo:any){
+        this.message = msgInfo.message;
+    }
+}
